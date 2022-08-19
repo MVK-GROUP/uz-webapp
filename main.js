70863e67b6dee08fa76829a6a260c5b8b007be43
valeriistefanyk <valeriistefanyk@users.noreply.github.com>
2022-08-19 10:31:34 +0000

    deploy: dccebe70fe9e5cf3e534574c6b5e80d62a9bb18a

---
+++ main.js
@@ -0,0 +1,23 @@
+function openLiqpay(data, signature, isBlank) {
+    var mapForm = document.createElement("form");
+    if (isBlank) {
+        mapForm.target = "_blank"
+    }
+    mapForm.method = "POST";
+    mapForm.action = "https://www.liqpay.ua/api/3/checkout";
+
+    var dataInput = document.createElement("input");
+    dataInput.type = "text";
+    dataInput.name = "data";
+    dataInput.value = data;
+    mapForm.appendChild(dataInput);
+
+    var signatureInput = document.createElement("input");
+    signatureInput.type = "text";
+    signatureInput.name = "signature";
+    signatureInput.value = signature;
+    mapForm.appendChild(signatureInput);
+
+    document.body.appendChild(mapForm);
+    mapForm.submit();
+}
